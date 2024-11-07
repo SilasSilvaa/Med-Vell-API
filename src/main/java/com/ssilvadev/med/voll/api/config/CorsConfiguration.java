d4ef90e771b2df9bad02a12f490c6295e22f54cd
SilasSilvaa <silas-oli@hotmail.com>
2024-11-07 00:28:56 -0300

    config - cors configuration

--- src/main/java/com/ssilvadev/med/voll/api/config/CorsConfiguration.java
+++ src/main/java/com/ssilvadev/med/voll/api/config/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package com.ssilvadev.med.voll.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
