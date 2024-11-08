17c1b5cefa85aac421fe3f0ea64b95cd6164deae
SilasSilvaa <silas-oli@hotmail.com>
2024-11-08 01:23:19 -0300

    feat - entity Patient for mapping data on database

---
+++ src/main/java/com/ssilvadev/med/voll/api/model/Patient.java
@@ -0,0 +1,36 @@
+package com.ssilvadev.med.voll.api.model;
+
+import com.ssilvadev.med.voll.api.dto.PatientRegisterDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Patient")
+@Table(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String cpf;
+    private String phone;
+
+    @Embedded
+    private Address address;
+
+    public Patient(PatientRegisterDTO data) {
+        this.name = data.name();
+        this.email = data.email();
+        this.phone = data.phone();
+        this.cpf = data.cpf();
+        this.address = new Address(data.address());
+    }
+}
