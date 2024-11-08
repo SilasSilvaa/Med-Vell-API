81629553c890f082a3af11dd44b054859d7c1dc6
SilasSilvaa <silas-oli@hotmail.com>
2024-11-08 01:21:41 -0300

    feat - dto for register new Doctor

--- src/main/java/com/ssilvadev/med/voll/api/dto/DoctorRegisterDataDTO.java
+++ src/main/java/com/ssilvadev/med/voll/api/dto/DoctorRegisterDataDTO.java
@@ -1,7 +1,17 @@
 package com.ssilvadev.med.voll.api.dto;
 
 import com.ssilvadev.med.voll.api.enums.Specialty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record DoctorRegisterDataDTO(String name, String email,
-                                    String crm, Specialty specialty, Address address) {
+public record DoctorRegisterDataDTO(
+        @NotBlank String name,
+        @NotBlank @Email String email,
+        @NotBlank String phone,
+        @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
+        @NotNull Specialty specialty,
+        @NotNull @Valid AddressDTO address) {
 }
