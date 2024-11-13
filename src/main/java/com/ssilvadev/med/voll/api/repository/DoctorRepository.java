6823d69a65326a93280b13b9381576a2cc136f5b
SilasSilvaa <silas-oli@hotmail.com>
2024-11-13 01:44:14 -0300

    feat - add new method for find all doctors by status equals true

--- src/main/java/com/ssilvadev/med/voll/api/repository/DoctorRepository.java
+++ src/main/java/com/ssilvadev/med/voll/api/repository/DoctorRepository.java
@@ -1,7 +1,11 @@
 package com.ssilvadev.med.voll.api.repository;
 
 import com.ssilvadev.med.voll.api.model.Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+    Page<Doctor> findAllByStatusTrue(Pageable pageable);
 }
