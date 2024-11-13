89d58009ddbf01ff28e39eb79a144019d7c993f0
SilasSilvaa <silas-oli@hotmail.com>
2024-11-13 01:42:22 -0300

    feat - add new request methods (GetByID, Put, Delete) for Doctor controller

--- src/main/java/com/ssilvadev/med/voll/api/controller/DoctorController.java
+++ src/main/java/com/ssilvadev/med/voll/api/controller/DoctorController.java
@@ -1,15 +1,19 @@
 package com.ssilvadev.med.voll.api.controller;
 
-import com.ssilvadev.med.voll.api.dto.DoctorRegisterDataDTO;
-import com.ssilvadev.med.voll.api.dto.ListDoctorData;
+import com.ssilvadev.med.voll.api.dto.*;
 import com.ssilvadev.med.voll.api.model.Doctor;
 import com.ssilvadev.med.voll.api.repository.DoctorRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("api/medicos")
@@ -20,13 +24,43 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid DoctorRegisterDataDTO data){
-        repository.save(new Doctor(data));
+    public ResponseEntity<DoctorDetailDTO> register(@RequestBody @Valid DoctorRegisterDataDTO data, UriComponentsBuilder builder){
+        var doctor = repository.save(new Doctor(data));
+        var uri = builder.path("/api/medicos/{id}").buildAndExpand(doctor.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DoctorDetailDTO(doctor));
     }
 
     @GetMapping
-    public List<ListDoctorData> getDoctors(){
-        return repository.findAll().stream()
-                .map(ListDoctorData::new).toList();
+    public ResponseEntity<Page<ListDoctorDTO>> getDoctors(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable){
+        var doctors = repository.findAllByStatusTrue(pageable).map(ListDoctorDTO::new);
+        return ResponseEntity.ok(doctors);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorDetailDTO> getDoctor(@PathVariable Long id){
+        var getDoctor = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DoctorDetailDTO(getDoctor));
+
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<DoctorDetailDTO> update(@RequestBody @Valid DoctorUpdateDataDTO data){
+        var doctor = repository.getReferenceById(data.id());
+        doctor.updateInfo(data);
+
+        return ResponseEntity.ok(new DoctorDetailDTO(doctor));
+
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity delete(@PathVariable Long id){
+        var doctor = repository.getReferenceById(id);
+        doctor.inactivate();
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
