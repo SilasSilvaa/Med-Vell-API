05dc4ea38593e6a46f19a6a7d5983e9ea6a39396
SilasSilvaa <silas-oli@hotmail.com>
2024-11-20 19:07:06 -0300

    refactor - add new method to find user by status

--- src/main/java/com/ssilvadev/med/voll/api/repository/PatientRepository.java
+++ src/main/java/com/ssilvadev/med/voll/api/repository/PatientRepository.java
@@ -2,6 +2,14 @@ package com.ssilvadev.med.voll.api.repository;
 
 import com.ssilvadev.med.voll.api.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    @Query("""
+            select p.status
+            from Patient p
+            where
+            p.id = :patientId
+            """)
+    boolean findStatusById(Long patientId);
 }
